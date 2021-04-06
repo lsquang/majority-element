4d82e13417012024417b20be7abacdd505e1ecd3
oliule2010 <oliule2010@gmail.com>
2021-04-06 20:03:52 +0100

    Fast version

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -16,7 +17,7 @@ int count_appear(const vector<int>& numbers, int n) {
 }
 
 
-int most_frequence_value(const vector<int>& numbers) {
+int majorityElement(const vector<int>& numbers) {
     for (int value : numbers) {
         if (count_appear(numbers, value) > numbers.size() / 2) {
             return value;
@@ -25,9 +26,32 @@ int most_frequence_value(const vector<int>& numbers) {
     return -1;
 }
 
+
+int majorityElementFast(const vector<int>& numbers) {
+    unordered_map<int, int> counts;
+    for (int value : numbers) {
+        counts[value] = 0;
+    }
+
+    for (int value : numbers) {
+        counts[value] ++;
+    }
+
+    for (int value : numbers) {
+        if (counts[value] > numbers.size()/ 2) {
+            return value;
+        }
+    }
+    return -1;
+}
+
+
+
+
+
 int main()
 {
     vector<int> numbers = { 2, 2, 1, 1, 1, 2, 2 };
-    cout << most_frequence_value(numbers) << endl;
+    cout << majorityElement(numbers) << endl;
 }
  
